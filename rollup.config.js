79c1bc160f810c020022c53ceeed90daf686d502
javabryan <bryan.patrick90@gmail.com>
2021-04-10 02:12:34 -0500

    Added more structure and a Timer class

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from "@rollup/plugin-babel";
+import postcss from "rollup-plugin-postcss";
+import flexbugs from "postcss-flexbugs-fixes";
+import postcssPresetEnv from "postcss-preset-env";
+import { terser } from "rollup-plugin-terser";
+
+const addPlugins = () => {
+  return [
+    postcss({
+      extract: true,
+      minimize: true,
+      use: ["sass"],
+      plugins: [flexbugs(), postcssPresetEnv()],
+    }),
+  ];
+};
+// rollup.config.js
+export default [
+  {
+    input: "./src/sass/main.scss",
+    output: {
+      name: "MainStyles",
+      file: "./build/styles.min.css",
+      format: "es",
+    },
+    plugins: addPlugins(),
+  },
+  {
+    input: "./src/scripts/main.js",
+    output: {
+      file: "./build/index.min.js",
+      name: "MainScript",
+      format: "iife",
+    },
+    plugins: [babel({ babelHelpers: "bundled" }), terser],
+  },
+];
---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+import Timer from "./Timer";
+
+const $slotsArr = [];
+const $slots = document.getElementsByClassName("slots");
+
+for (i = 0; i < $slots.length; i++) {}
