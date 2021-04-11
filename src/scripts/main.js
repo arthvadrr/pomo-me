2e5f927a45f9c284db9e4699df6bb28af9c0174c
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2021-04-11 02:31:02 -0500

    Clockwerk

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,45 @@
-import Timer from "./Timer";
+const $timer = document.getElementById("timer");
+const $start = document.getElementById("start");
+const $stop = document.getElementById("stop");
+let isRunning = false;
+let dateStarted;
+let delta;
+let countdown = () => {};
 
-const $slotsArr = [];
-const $slots = document.getElementsByClassName("slots");
+let startTime = 30;
+let remainingTime = 30;
+let currentDelta;
 
-for (i = 0; i < $slots.length; i++) {}
+const adjustTime = (delta) => {
+    const res = startTime - delta;
+
+    if (res === 0) {
+        delta = 0;
+    }
+
+    return startTime - delta;
+};
+
+const onStart = () => {
+    if (!isRunning) {
+        isRunning = true;
+        $start.innerHTML = "Pause";
+        dateStarted = Date.now();
+        countdown = setInterval(() => {
+            delta = Math.round((Date.now() - dateStarted) / 1000);
+            remainingTime = adjustTime(delta);
+            $timer.innerHTML = remainingTime.toString();
+            if (remainingTime === 0) {
+                clearInterval(countdown);
+                return;
+            }
+        }, 100);
+    } else {
+        isRunning = false;
+        $start.innerHTML = "Start";
+        startTime = remainingTime;
+        clearInterval(countdown);
+    }
+};
+
+$start.addEventListener("click", onStart);
