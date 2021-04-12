8e1b3db52465e514080ad63d8032f9ac63a4659f
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2021-04-11 23:52:59 -0500

    Timer logic

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Pomo Me
+
+A pomo web app
+
+## Cloning/Installation
+
+Rollup is compiling SCSS and JS from /src to /build.
+
+To get started, use NPM to install the required modules and start the watcher and build script (npm start)
+
+<code>npm i</code>
+<code>npm start</code>
+
+### Rollup Plugins
+
+Babel, PostCSS, FlexBugs, PostCSS Preset Env, Terser
+
+## Copyright
+
+Pomo Me is under GPL-2.0-or-later. It means its free, do what you want with it.
+Check out the official lawyer stuff here https://www.gnu.org/licenses/licenses.html
+
+## A note about PR
+
+Feel free to submit PRs, I <i>will</i> look at them.
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,96 @@
 const $timer = document.getElementById("timer");
 const $start = document.getElementById("start");
-const $stop = document.getElementById("stop");
+const $reset = document.getElementById("reset");
 let isRunning = false;
 let dateStarted;
 let delta;
 let countdown = () => {};
 
-let startTime = 30;
-let remainingTime = 30;
+let originalTime = 36000000000;
+let startTime = originalTime;
+let remainingTime = originalTime;
 let currentDelta;
 
-const adjustTime = (delta) => {
+const formatTime = seconds => {
+    let formattedTime;
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+
+    let fseconds = seconds % 60;
+    let fminutes = minutes % 60;
+    let fhours = hours % 99;
+
+    const addLeadingZero = int => (int < 10 ? `0${int}` : int);
+
+    if (fhours > 99) {
+        fhours = 99;
+    }
+
+    fseconds = addLeadingZero(fseconds);
+    fminutes = addLeadingZero(fminutes);
+    fhours = addLeadingZero(fhours);
+
+    formattedTime = `${fminutes}:${fseconds}`;
+
+    return formattedTime;
+};
+
+const printTimer = print => ($timer.innerHTML = print);
+
+const adjustTime = delta => {
     const res = startTime - delta;
 
     if (res === 0) {
         delta = 0;
+        return 0;
     }
 
     return startTime - delta;
 };
 
+const onReset = () => {
+    onStop();
+    $start.removeAttribute("disabled", true);
+    startTime = originalTime;
+    remainingTime = originalTime;
+    printTimer(formatTime(remainingTime));
+};
+
+const onStop = () => {
+    isRunning = false;
+    $start.innerHTML = "Start";
+    clearInterval(countdown);
+    startTime = remainingTime;
+    console.log(remainingTime);
+
+    if (remainingTime === 0) {
+        $start.setAttribute("disabled", true);
+    }
+};
+
 const onStart = () => {
     if (!isRunning) {
+        if (remainingTime === 0) {
+            remainingTime = startTime;
+        }
+
         isRunning = true;
         $start.innerHTML = "Pause";
         dateStarted = Date.now();
+
         countdown = setInterval(() => {
             delta = Math.round((Date.now() - dateStarted) / 1000);
             remainingTime = adjustTime(delta);
-            $timer.innerHTML = remainingTime.toString();
+            printTimer(formatTime(remainingTime));
+            console.log(remainingTime);
             if (remainingTime === 0) {
-                clearInterval(countdown);
-                return;
+                onStop();
             }
         }, 100);
-    } else {
-        isRunning = false;
-        $start.innerHTML = "Start";
-        startTime = remainingTime;
-        clearInterval(countdown);
+    } else if (remainingTime != 0) {
+        onStop();
     }
 };
 
 $start.addEventListener("click", onStart);
+$reset.addEventListener("click", onReset);
