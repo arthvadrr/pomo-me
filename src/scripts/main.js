1e8a131e605abd1a221d17c8e2c6e47d7375be23
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2021-04-13 01:08:18 -0500

    Timer logic makes sense

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,24 @@
+/*
+# Complaints/ TODO Refactor
+## Utilize state
+## Refactor to class
+## Purify functions
+## MVC refactor. MVC can be fully deployed here
+*/
+
 const $timer = document.getElementById("timer");
 const $start = document.getElementById("start");
 const $reset = document.getElementById("reset");
+const $increaseTime = document.getElementById("increaseTime");
+const $decreaseTime = document.getElementById("decreaseTime");
 let isRunning = false;
 let dateStarted;
 let delta;
+
+//JS or linter bug resulting in type error. TODO Investigate why I'm having to explicitly force type inference.
 let countdown = () => {};
 
-let originalTime = 36000000000;
+let originalTime = 1500;
 let startTime = originalTime;
 let remainingTime = originalTime;
 let currentDelta;
@@ -14,23 +26,21 @@ let currentDelta;
 const formatTime = seconds => {
     let formattedTime;
     let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
 
     let fseconds = seconds % 60;
     let fminutes = minutes % 60;
-    let fhours = hours % 99;
+
+    if (minutes >= 60) {
+        fminutes = 60;
+        fseconds = 0;
+    }
 
     const addLeadingZero = int => (int < 10 ? `0${int}` : int);
 
-    if (fhours > 99) {
-        fhours = 99;
-    }
-
     fseconds = addLeadingZero(fseconds);
     fminutes = addLeadingZero(fminutes);
-    fhours = addLeadingZero(fhours);
 
-    formattedTime = `${fhours}:${fminutes}:${fseconds}`;
+    formattedTime = `${fminutes}:${fseconds}`;
 
     return formattedTime;
 };
@@ -92,6 +102,32 @@ const onStart = () => {
     }
 };
 
+const onIncreaseTime = () => {
+    isRunning = false;
+    clearInterval(countdown);
+    $start.innerHTML = "Start";
+
+    if (startTime <= 3300) {
+        startTime += 300;
+    } else {
+        startTime = 3600;
+    }
+    printTimer(formatTime(startTime));
+};
+
+const onDecreaseTime = () => {
+    isRunning = false;
+    clearInterval(countdown);
+    $start.innerHTML = "Start";
+
+    if (startTime >= 600) {
+        startTime -= 300;
+        printTimer(formatTime(startTime));
+    }
+};
+
 $start.addEventListener("click", onStart);
 $reset.addEventListener("click", onReset);
+$increaseTime.addEventListener("click", onIncreaseTime);
+$decreaseTime.addEventListener("click", onDecreaseTime);
 printTimer(formatTime(remainingTime));
