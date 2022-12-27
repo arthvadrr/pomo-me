5c39da9daa2cd3d20e6d08bd2a92d4ec246ebfd6
Bryan Patrick <bpatrick@ou.edu>
2022-12-27 14:20:59 -0600

    Updated rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,18 @@
-import babel from '@rollup/plugin-babel';
-import postcss from 'rollup-plugin-postcss';
-import flexbugs from 'postcss-flexbugs-fixes';
-import postcssPresetEnv from 'postcss-preset-env';
-import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel'
+import postcss from 'rollup-plugin-postcss'
+import flexbugs from 'postcss-flexbugs-fixes'
+import postcssPresetEnv from 'postcss-preset-env'
+import terser from '@rollup/plugin-terser'
 
-const addPlugins = () => {
-    return [
-        postcss({
-            extract: true,
-            minimize: true,
-            use: ['sass'],
-            plugins: [flexbugs(), postcssPresetEnv()],
-        }),
-    ];
-};
+const addPlugins = () => [
+    postcss({
+        extract: true,
+        minimize: true,
+        use: ['sass'],
+        plugins: [flexbugs(), postcssPresetEnv()],
+    }),
+];
+
 // rollup.config.js
 export default [
     {
